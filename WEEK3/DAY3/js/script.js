906ae4a1cf46c1253f684e5d047010580197d9d4
Pietroro98 <pietroro98@gmail.com>
2024-07-24 17:07:21 +0200

    unit2week3day3

--- WEEK3/DAY3/js/script.js
+++ WEEK3/DAY3/js/script.js
@@ -26,12 +26,13 @@ function loadCard() {
     const card = document.createElement("div");
     card.classList.add("col");
     card.innerHTML += `
-  <div class="card h-100">
+  <div class="card w-100">
   <img src="${book.img}" class="card-img-top" alt="${book.title}">
   <div class="card-body">
   <h3 class='card-title'>${book.title}</h3>
     <p class="card-text">${book.price} €</p>
     <button class="btn btn-danger" onclick="deleteCard(${index})" >SCARTA</button>
+    <button class="btn btn-success" onclick="buyNowCard(${index})" >COMPRA</button>
   </div>
 </div>`;
     boookListContainer.appendChild(card);
