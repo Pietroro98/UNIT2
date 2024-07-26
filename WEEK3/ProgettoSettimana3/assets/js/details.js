1962e4008562dcbed18b6eb7140d10463e93198c
Pietroro98 <pietroro98@gmail.com>
2024-07-26 16:18:57 +0200

    UNIT2 progettosettimana3

---
+++ WEEK3/ProgettoSettimana3/assets/js/details.js
@@ -0,0 +1,84 @@
+const addressBarParameters = new URLSearchParams(location.search);
+const productId = addressBarParameters.get("productId");
+console.log("productId", productId);
+
+const apiURL = "https://striveschool-api.herokuapp.com/api/product/";
+const token =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmEzNjE4MGYyNjBjYzAwMTVjYzBkZWIiLCJpYXQiOjE3MjE5ODMzNjAsImV4cCI6MTcyMzE5Mjk2MH0.4HMYcaTQzylGef5SY9hmzHNlwkBHL5h8TzDLTSJCvfU";
+
+// Funzione per mostrare i dettagli del prodotto
+function showProductDetails(product) {
+  const detailRow = document.getElementById("detail-row");
+  detailRow.innerHTML = `
+    <div class="col mb-4">
+      <div class="card w-50">
+        <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
+        <div class="card-body">
+          <h5 class="card-title">${product.name}</h5>
+          <hr />
+          <p class="card-text">${product.brand}</p>
+          <hr />
+          <p class="card-text">${product.description}</p>
+          <p class="card-text">${product.price}€</p>
+          <hr />
+          <a href="./backoffice.html?productId=${product._id}" class="btn btn-warning me-2">Edit</a>
+          <button class="btn btn-danger" id="delete-button">Delete</button>
+        </div>
+      </div>
+    </div>
+  `;
+
+  // Aggiungi l'evento click al pulsante di eliminazione
+  document
+    .getElementById("delete-button")
+    .addEventListener("click", deleteProduct);
+}
+
+// Funzione per recuperare i dettagli del prodotto
+function fetchProductDetails() {
+  fetch(apiURL + productId, {
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Errore nel recupero del prodotto");
+      }
+    })
+    .then((product) => {
+      console.log("Product:", product);
+      showProductDetails(product);
+    })
+    .catch((err) => {
+      console.error("Errore:", err);
+      alert("Impossibile recuperare i dettagli del prodotto.");
+    });
+}
+
+// Funzione per eliminare il prodotto
+function deleteProduct() {
+  fetch(apiURL + productId, {
+    method: "DELETE",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        alert("Prodotto eliminato");
+        location.assign("./index.html"); // Riporta l'utente alla homepage
+      } else {
+        throw new Error("Problema nell'eliminazione del prodotto");
+      }
+    })
+    .catch((err) => {
+      console.error("Errore:", err);
+      alert("Impossibile eliminare il prodotto.");
+    });
+}
+
+// Avvia il recupero dei dettagli del prodotto
+fetchProductDetails();
--- WEEK3/ProgettoSettimana3/assets/js/script.js
+++ WEEK3/ProgettoSettimana3/assets/js/script.js
@@ -1,5 +1,5 @@
 const getProducts = function () {
-  const URL = 'https://striveschool-api.herokuapp.com/api/product/'
+  const URL = "https://striveschool-api.herokuapp.com/api/product/";
   const token =
     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmEzNjE4MGYyNjBjYzAwMTVjYzBkZWIiLCJpYXQiOjE3MjE5ODMzNjAsImV4cCI6MTcyMzE5Mjk2MH0.4HMYcaTQzylGef5SY9hmzHNlwkBHL5h8TzDLTSJCvfU";
 
@@ -17,18 +17,27 @@ const getProducts = function () {
     })
     .then((products) => {
       const cartContainer = document.getElementById("cartContainer");
-      cartContainer.innerHTML = ""; 
-      
+      cartContainer.innerHTML = "";
+
       products.forEach((product) => {
         const productCol = `
-          <div class="col mb-4">
+        <div class="col-12 col-md-4 col-lg-3 mb-4">
             <div class="card h-100">
-              <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
-              <div class="card-body">
-                <h5 class="card-title">${product.name}</h5>
-                <p class="card-text">${product.description}</p>
-                <p class="card-text">$${product.price}</p>
-                <a href="./details.html?productId=${product._id}" class="btn btn-primary">View Details</a>
+              <div class="card-img-container">
+                <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
+              </div>
+              <div class="card-body d-flex flex-column">
+                <div class="card-title-container">
+                  <h5 class="card-title">${product.name}</h5>
+                  <hr />
+                  <p class="card-text">${product.brand}</p>
+                </div>
+                <div class="card-text-container">
+                  <p class="card-text mt-0 mb-2">${product.price}€</p>
+                </div>
+                <div class="card-footer-container mt-auto">
+                  <a href="./details.html?productId=${product._id}" class="btn btn-primary">View Details</a>
+                </div>
               </div>
             </div>
           </div>
@@ -42,3 +51,32 @@ const getProducts = function () {
 };
 
 getProducts();
+
+/* <p class="card-text">${product.description}</p>
+
+            <div class="col">
+                <div class="card">
+                    <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">${product.name}</h5>
+                        
+                        <p class="card-text">$${product.price}</p>
+                         <a href="./details.html?productId=${product._id}" class="btn btn-primary">View Details</a>
+                    </div>
+                </div>
+            </div>
+            
+            
+            
+            
+               <div class="col mb-4">
+            <div class="card h-100">
+              <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
+              <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                
+                <p class="card-text">$${product.price}</p>
+                <a href="./details.html?productId=${product._id}" class="btn btn-primary">View Details</a>
+              </div>
+            </div>
+          </div>*/
